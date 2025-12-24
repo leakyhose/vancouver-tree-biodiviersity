c731ee756818cec76e0b2db4e74a21d173653b6c
unknown <yimingsu2007@gmail.com>
2025-12-23 18:41:25 -0800

    Modified to make suitable for importing into other modules

--- scripts/species_effective_count.py
+++ scripts/species_effective_count.py
@@ -5,17 +5,19 @@ import matplotlib.pyplot as plt
 from shapely.geometry import shape
 import json
 
-df = pd.read_csv("./data/public-trees.csv", sep=";")
 
-df["geom"] = df["geom"].apply(lambda x: shape(json.loads(x)))
-gdf = gpd.GeoDataFrame(df, geometry="geom", crs="EPSG:4326")
-
-def effective_species_count(species_series):
-    counts = species_series.value_counts()
+def effective_species_count(species):
+    counts = species.value_counts()
     p = counts / counts.sum()
     H = -(p * np.log(p)).sum()           
     effective_species = np.exp(H)        
     return effective_species
 
-city_effective_species = effective_species_count(gdf['SPECIES_NAME'])
-print(city_effective_species)
+if __name__ == "__main__":
+    
+    df = pd.read_csv("./data/public-trees.csv", sep=";")
+    df["geom"] = df["geom"].apply(lambda x: shape(json.loads(x)))
+    gdf = gpd.GeoDataFrame(df, geometry="geom", crs="EPSG:4326")
+
+    city_effective_species = effective_species_count(gdf['SPECIES_NAME'])
+    print(city_effective_species)
